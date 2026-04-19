290e90d96ed411ee3d229a1ae2b07445f63f450b
Prince <chakusaprince@gmail.com>
2026-04-19 13:25:48 +0400

    Add server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+﻿require('dotenv').config();
+const app = require('./src/app');
+const http = require('http');
+
+const server = http.createServer(app);
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(Server running on port );
+});
