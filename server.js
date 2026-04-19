6cd49a700237330c5913026c25578750e0d0c9a5
Prince <chakusaprince@gmail.com>
2026-04-19 13:21:14 +0400

    Clean server.js file

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+﻿require('dotenv').config();
 const app = require('./src/app');
 const http = require('http');
 
@@ -6,5 +6,5 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(Server running on port );
 });
