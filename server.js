ca3e466644c5d982784ac63a2ac6265ceadcffdc
Prince <chakusaprince@gmail.com>
2026-04-19 13:30:38 +0400

    Add correct server.js

--- server.js
+++ server.js
@@ -6,5 +6,6 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-  console.log(Server running on port );
+  // Use backticks (`) for template literals to include the PORT variable
+  console.log(`Server running on port ${PORT}`);
 });
